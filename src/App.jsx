d9e5c01f3aaa9c186799b279e764b73534184f52
Buertey Accam <manreg814@gmail.com>
2025-01-25 03:12:14 +0000

    feat: implement color toggle feature and update app title and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
+  const [ isChangeColor, setIsChangeColor ] = useState(false);
+
+    //get the changes from the browser
+    useEffect(()=>{
+      const storeColor = localStorage.getItem("isChangeColor");
+      if(storeColor){
+        setIsChangeColor(JSON.parse(storeColor))
+      }
+    },[])
+
+  //set the changes into the browser
+  useEffect(()=>{
+    localStorage.setItem("isChangeColor", JSON.stringify(isChangeColor))
+
+  }, [isChangeColor])
+
+  const handleChangeColor = ()=> {
+    setIsChangeColor(!isChangeColor);
+
+  }
+
   return (
-    <div>
-      <h1>My App</h1>
+    <div className={ isChangeColor ? `white-bg` : `dark-bg` }>
+      <h1 className="header" onClick={handleChangeColor}>My App</h1>
     </div>
   )
 }
