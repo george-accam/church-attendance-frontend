109af2dbf610ec95c4d80de0d258d2084185d9d7
Buertey Accam <manreg814@gmail.com>
2025-01-26 06:33:09 +0000

    feat: enhance LandingPage with new images, marquee text, and improved layout; update styling for better user experience

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import Navbar from "../Navbar";
+import LandImg1 from "../assets/land-img1.jpeg"
+import LandImg2 from "../assets/land-img2.jpeg"
+import LandImg3 from "../assets/land-img3.jpeg"
+import LandImg4 from "../assets/land-img4.jpeg"
+import LandImg6 from "../assets/land-img6.jpeg"
 
 const LandingPage = () => {
     return (
@@ -13,8 +18,38 @@ const LandingPage = () => {
                                 Welcome to the Church Attendance System!
                             </h1>
                             <h2 className='landing-page-title2'>
-                                Effortless attendance tracking and member management at your fingertips.
+                                <marquee behavior="scroll" direction="left">
+                                    Effortless attendance tracking and member management at your fingertips.
+                                </marquee>
                             </h2>
+                            <div className="landing-page-sub-image">
+                                <div className="sub-image-container1 sub-image-top">
+                                    <img src={LandImg1} alt="" />
+                                    <span>Hello</span>
+                                </div>
+                                <div className="sub-image-container1">
+                                    <img src={LandImg2} alt="" />
+                                    <span>Hello</span>
+                                </div>
+                                <div className="sub-image-container1 sub-image-top">
+                                    <img src={LandImg3} alt="" />
+                                    <span>Hello</span>
+                                </div>
+                                <div className="sub-image-container1">
+                                    <img src={LandImg4} alt="" />
+                                    <span>Hello</span>
+                                </div>
+                                <div className="sub-image-container1 sub-image-top">
+                                    <img src={LandImg6} alt="" />
+                                    <span>Hello</span>
+                                </div>
+                            </div>
+                                <div className="landing-page-bottom">
+                                    <h3>Get Started Today!</h3>
+                                    <p>
+                                        Sign up now and experience the convenience of modern church management.
+                                    </p>
+                                </div>
                         </div>
                     </div>
                 </div>
