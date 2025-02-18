4cdb6ad0126ed677df094c8fc9b28d1c7db20811
Buertey Accam <manreg814@gmail.com>
2025-02-18 18:53:36 +0000

    feat: add Delete and Rename components and adjust delete icon margin in App.css

---
+++ src/components/reusableComponents/Delete.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Delete = ({ member }) => {
+    return (
+        <div>
+            Delete
+        </div>
+    )
+}
+
+export default Delete;
---
+++ src/components/reusableComponents/Rename.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rename = () => {
+  return (
+    <div>
+        Rename
+    </div>
+  )
+}
+
+export default Rename;
