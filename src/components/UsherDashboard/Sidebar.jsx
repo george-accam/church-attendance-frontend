bcad977173d33c475463742cf6086578d1426c78
Buertey Accam <manreg814@gmail.com>
2025-02-24 06:17:42 +0000

    feat: enhance sidebar navigation with icons and improve button loading states

--- src/components/UsherDashboard/Sidebar.jsx
+++ src/components/UsherDashboard/Sidebar.jsx
@@ -1,3 +1,9 @@
+import { RiLogoutCircleLine } from "react-icons/ri"; 
+import { MdEmail } from "react-icons/md"; 
+import { BsFillCalendarCheckFill } from "react-icons/bs"; 
+import { FaRegistered } from "react-icons/fa"; 
+import { BsFillPersonFill } from "react-icons/bs"; 
+import { IoIosPeople } from "react-icons/io"; 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Logout from '../reusableComponents/Logout';
@@ -56,33 +62,54 @@ const Sidebar = ({ userName, userEmail, sidebarActive, setSidebarActive, handleS
             onClick={()=> handleShowActive("all-members")} 
             className={`sidebar-navigator ${isActive === "all-members" ? "active" : ""}`}
             >
+              <div className="sidebar-navigator-list">
+              <IoIosPeople className="navigator-icon" />
                 All members
+              </div>
             </Link>
 
             <Link to="/usher-dashboard/personal"
             onClick={()=> handleShowActive("personal")} 
-            className={`sidebar-navigator ${isActive === "personal" ? "active" : ""}`}>
+            className={`sidebar-navigator ${isActive === "personal" ? "active" : ""}`}
+            >
+              <div className="sidebar-navigator-list">
+              <BsFillPersonFill className="navigator-icon" />
                 Personal
+              </div>
             </Link>
 
             <Link to="/usher-dashboard/register-member"
             onClick={()=> handleShowActive("register")} 
-            className={`sidebar-navigator ${isActive === "register" ? "active" : ""}`}>
+            className={`sidebar-navigator ${isActive === "register" ? "active" : ""}`}
+            >
+              <div className="sidebar-navigator-list">
+              <FaRegistered className="navigator-icon" />
                 Register
+              </div>
             </Link>
 
             <Link to="/usher-dashboard/check-in"
             onClick={()=> handleShowActive("check-in")} 
-            className={`sidebar-navigator ${isActive === "check-in" ? "active" : ""}`}>
+            className={`sidebar-navigator ${isActive === "check-in" ? "active" : ""}`}
+            >
+              <div className="sidebar-navigator-list">
+              <BsFillCalendarCheckFill className="navigator-icon" />
                 Check In
+              </div>
             </Link>
           </div>
           <div className="sidebar-bottom">
             <p className="sidebar-email">
-              email:  <span>{ userEmail }</span>
+            <div className="sidebar-navigator-list">
+              <MdEmail className="email-icon" />:  
+              <span>{ userEmail }</span>
+            </div>
             </p>
             <p onClick={handleLogout} className="sidebar-logout">
+              <div className="sidebar-navigator-list">
+                <RiLogoutCircleLine className="logout-sidebar-icon" />
                 Logout
+              </div>
             </p>
           </div>
         </div>
